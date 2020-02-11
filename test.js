24d8d45a1a520a65e618efc2dfba2a4ae40d9c4a
liuzemei <her_liu@qq.com>
2020-02-11 17:39:45 +0800

    1

--- test.js
+++ test.js
@@ -9,6 +9,7 @@ let client = new Mixin({
     "private_key": "-----BEGIN RSA PRIVATE KEY-----\r\nMIICXQIBAAKBgQCeeICSpTfJu9RNVqc5QHpUgU1jLq7D70ngPY49+vlh1j/aEEvw\r\nmUnPyWfuFvlYl1nahjHnkDtzOwVqNVHZbM+ec1l/+JrXFtdLqAOA5EJgjfJs1/pU\r\n+amMw38+64awHZfvhjZjAiL6Uodvjyf2f71lSeKcgjAPWLX7dSfHaDihuQIDAQAB\r\nAoGAZZ9q/B99Xh3YcFwQUrOZfxWINgFay4Vo/6cZz39hay9EtSdcO4dhgSFsR49u\r\nCTTNx0flOtUQer2vpENbIK4ZmUTa8XHICAkJRhHUCk36pywPeSOiGQO9MoAHlrt5\r\nVbQ9cfX+1A6wsxlmC4HfG2TMjCgR8i3zcSRtL6CLbWwnAzECQQDZHPn0iqn3+7H0\r\ncJLFMbX7+s/yQMg+leVB9I9Oy/PckmR3S1ynNB1pdevUj/z8vrU+abrNCjjDmp8M\r\nSfP59woVAkEAutqqIgxBKqrA4VZekrim398OuzllhA11PsGFgjUYxYZ4jNbTX6+O\r\nnjk0yS26axlWZSLF8alLYmUF49/UhHsWFQJAcgfzDEpMDTAH/j+mBqYeU/MehrkZ\r\nsy1dVO/S2Orr8Px+gd12E2eezwYdT+40TLcJUHeNoiVtPOcYBAdPegkTJQJBAK8F\r\noTURO31GwjKQHzfZhF3QZtESOzeND5R/2pmTOKfUhFiP5SAMpk8okHwcvDATZKyI\r\n/NaW2AjLTdAb2claLd0CQQDEDHW4zeCI2elWhoHpdFLZqqH/UnqUA4nELGwpARGr\r\nH1wJjI7J7veKR0TxKX6xll8knnWo2SIN67Sj4QAHEiF9\r\n-----END RSA PRIVATE KEY-----\r\n"
 })
 
+
 let recipient_id = 'e8e8cd79-cd40-4796-8c54-3a13cfe50115'
 let data = `
 ## liuzemei
